46359737acedf7b163dd07c541204cc3ecb88a42
Daniel Büchele <daniel@buechele.cc>
2020-09-19 22:05:42 +0100

    adding apollo-ios

--- iOS/App/FeedShare/ContentView.swift
+++ iOS/App/FeedShare/ContentView.swift
@@ -5,11 +5,14 @@
 //  Created by Gabriel Knoll on 19.09.20.
 //
 
+import Network
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var launchData: LaunchListData = LaunchListData()
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(launchData.username)
             .padding()
     }
 }
@@ -17,5 +20,28 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class LaunchListData: ObservableObject {
+    @Published var username: String
+    
+    init() {
+        self.username = "I don't know who you are"
+        loadData()
+    }
+    
+    func loadData() {
+        Network.shared.apollo.fetch(query: ContentViewQuery()) { result in
+            switch result {
+            case .success(let graphQLResult):
+                if let handle = graphQLResult.data?.viewer?.user?.handle {
+                    self.username = handle
+                    print(handle)
+                }
+            case .failure(let error):
+                print("Failure! Error: \(error)")
+            }
+        }
     }
 }
---
+++ iOS/App/FeedShare/Network.swift
@@ -0,0 +1,33 @@
+//
+//  Network.swift
+//  FeedShare
+//
+//  Created by Daniel Büchele on 9/19/20.
+//
+
+import Apollo
+import Foundation
+
+final class Network {
+    static let shared = Network()
+    private lazy var networkTransport: HTTPNetworkTransport = {
+        let transport = HTTPNetworkTransport(url: URL(string: "https://feed.buechele.cc/graphql")!)
+        transport.delegate = self
+        
+        return transport
+    }()
+    
+    private(set) lazy var apollo = ApolloClient(networkTransport: self.networkTransport)
+}
+
+extension Network: HTTPNetworkTransportPreflightDelegate {
+    func networkTransport(_ networkTransport: HTTPNetworkTransport, shouldSend request: URLRequest) -> Bool {
+        return true
+    }
+    
+    func networkTransport(_ networkTransport: HTTPNetworkTransport, willSend request: inout URLRequest) {
+        var headers = request.allHTTPHeaderFields ?? [String: String]()
+        headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiMSJ9.c_rZK1KlP58-R51c2SfCv7JQYWfnSlU3whNvAw3-dqw"
+        request.allHTTPHeaderFields = headers
+    }
+}
