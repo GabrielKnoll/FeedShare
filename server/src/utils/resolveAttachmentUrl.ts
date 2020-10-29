225dfe005abdad2f40caac99b8629eb209c60c1e
Daniel Büchele <daniel@buechele.cc>
2020-10-29 00:38:24 +0000

    podcast parsers

--- server/src/utils/resolveAttachmentUrl.ts
+++ server/src/utils/resolveAttachmentUrl.ts
@@ -3,13 +3,28 @@ import {ampPodcast, ampEpisode} from './appleApi';
 import prismaClient from './prismaClient';
 import {PodcastCreateInput, EpisodeCreateInput} from '@prisma/client';
 import URL from 'url';
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
 
 const CACHE_DURATION = 24 * 60 * 60 * 1000;
+
+async function fetchPage(url: URL.UrlWithStringQuery) {
+  const pageContent = await fetch(url).then((res) => res.text());
+  return cheerio.load(pageContent);
+}
+
+function idFromAppleUrl(itunesUrl?: string): string | undefined {
+  if (itunesUrl && URL.parse(itunesUrl).hostname === 'podcasts.apple.com') {
+    const match = itunesUrl.match(/id(\d+)/) ?? [];
+    return match[1];
+  }
+}
 
 export default async function (shareUrl: string) {
   const url = URL.parse(shareUrl);
   let applePodcastId: string | undefined = undefined;
   let appleEpisodeId: string | undefined = undefined;
+  let rssFeed: string | undefined = undefined;
 
   switch (url.hostname) {
     case 'podcasts.apple.com': {
@@ -22,6 +37,74 @@ export default async function (shareUrl: string) {
       appleEpisodeId = (Array.isArray(id) ? id[0] : id) ?? undefined;
       break;
     }
+
+    case 'castro.fm': {
+      // https://castro.fm/podcast/fc60896e-f790-4357-98a8-b8901270a0f8
+      let [type, id] = (url.pathname ?? '').split('/').filter(Boolean);
+
+      if (type === 'itunes') {
+        // https://castro.fm/itunes/1172218725
+        applePodcastId = id;
+        break;
+      }
+
+      const $ = await fetchPage(url);
+      const overcastLink = $(
+        '#co-supertop-castro-open-in a[href^="https://overcast.fm/itunes"]',
+      );
+      if (overcastLink.length > 0) {
+        const podcastId =
+          overcastLink
+            .first()
+            .attr('href')
+            ?.match(/https:\/\/overcast\.fm\/itunes(\d+)/) ?? [];
+        applePodcastId = podcastId[1];
+      }
+
+      rssFeed = $('img[alt="Subscribe to RSS"]').parent().attr('href');
+
+      if (type === 'episode') {
+        // https://castro.fm/episode/XjciiV
+      }
+      break;
+    }
+
+    case 'overcast.fm': {
+      // https://overcast.fm/+HiEYDNrZs
+      // https://overcast.fm/itunes1357986673
+      // https://overcast.fm/itunes1357986673/tony-basilios-next-level-network-family-of-podcasts
+      let [itunes] = (url.pathname ?? '').split('/').filter(Boolean);
+      const podcastId = itunes.match(/^itunes(\d+)$/) ?? [];
+      applePodcastId = podcastId[1];
+
+      if (!applePodcastId) {
+        const $ = await fetchPage(url);
+        applePodcastId = idFromAppleUrl(
+          $('.externalbadges a[href^="https://podcasts.apple.com/podcast/"]')
+            .first()
+            .attr('href'),
+        );
+        rssFeed = $('img[src="/img/badge-rss.svg"]').parent().attr('href');
+      }
+      break;
+    }
+
+    case 'pca.st': {
+      // https://pca.st/itunes/1172218725
+      let [type, id] = (url.pathname ?? '').split('/').filter(Boolean);
+      if (type === 'itunes') {
+        applePodcastId = id;
+      } else {
+        // https://pca.st/63et
+        const $ = await fetchPage(url);
+        rssFeed = $('.rss_button a').first().attr('href');
+        applePodcastId = idFromAppleUrl(
+          $('.itunes_button a').first().attr('href'),
+        );
+      }
+      break;
+    }
+
     default: {
       throw new Error(`Unknown share URL: ${url}`);
     }
@@ -65,7 +148,7 @@ export default async function (shareUrl: string) {
     ampApiResponse: ampApiPodcast,
     title: ampApiPodcast.attributes.name,
     url: ampApiPodcast.attributes.websiteUrl,
-    feed: 'todo',
+    feed: rssFeed ?? '',
     publisher: ampApiPodcast.attributes.artistName,
     description: ampApiPodcast.attributes.description.standard,
     ampId: applePodcastId,
