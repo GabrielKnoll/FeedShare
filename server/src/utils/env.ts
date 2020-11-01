85b1483a5cf00f348e40adb8fb493cc694c92b3b
Daniel Büchele <daniel@buechele.cc>
2020-11-01 23:43:44 +0000

    require envin ci

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -1,29 +1,37 @@
 import env from 'env-var';
 const {parsed} = require('dotenv').config({path: __dirname + '/../../../.env'});
 
+const ci = env.get('CI').asBool();
+
 const e = {
-  DATABASE_URL: env.get('DATABASE_URL').required().asString(),
-  JWT_SECRET: env.get('JWT_SECRET').required().asString(),
-  PORT: env.get('PORT').required().asIntPositive(),
-  TWITTER_COMSUMER_KEY: env.get('TWITTER_COMSUMER_KEY').required().asString(),
+  DATABASE_URL: env.get('DATABASE_URL').required(!ci).asString(),
+  JWT_SECRET: env.get('JWT_SECRET').required(!ci).asString(),
+  PORT: env.get('PORT').required(!ci).asIntPositive(),
+  TWITTER_COMSUMER_KEY: env
+    .get('TWITTER_COMSUMER_KEY')
+    .required(!ci)
+    .asString(),
   TWITTER_CONSUMER_SECRET: env
     .get('TWITTER_CONSUMER_SECRET')
-    .required()
+    .required(!ci)
     .asString(),
-  SPOTIFY_CLIENT_ID: env.get('SPOTIFY_CLIENT_ID').required().asString(),
-  SPOTIFY_CLIENT_SECRET: env.get('SPOTIFY_CLIENT_SECRET').required().asString(),
+  SPOTIFY_CLIENT_ID: env.get('SPOTIFY_CLIENT_ID').required(!ci).asString(),
+  SPOTIFY_CLIENT_SECRET: env
+    .get('SPOTIFY_CLIENT_SECRET')
+    .required(!ci)
+    .asString(),
   APOLLO_KEY: env.get('APOLLO_KEY').asString(),
   APOLLO_GRAPH_VARIANT: env
     .get('APOLLO_GRAPH_VARIANT')
     .asEnum(['dev', 'current']),
   APOLLO_SCHEMA_REPORTING: env.get('APOLLO_SCHEMA_REPORTING').asBool(),
-  TIMBER_SOUCE_ID: env.get('TIMBER_SOUCE_ID').required().asString(),
-  TIMBER_TOKEN: env.get('TIMBER_TOKEN').required().asString(),
+  TIMBER_SOUCE_ID: env.get('TIMBER_SOUCE_ID').required(!ci).asString(),
+  TIMBER_TOKEN: env.get('TIMBER_TOKEN').required(!ci).asString(),
 };
 
 const set = new Set(Object.keys(parsed));
 Object.keys(e).forEach((key) => set.delete(key));
-if (set.size > 0) {
+if (set.size > 0 && !ci) {
   throw new Error(`${[...set].join(', ')} not declared in ${__filename}`);
 }
 
