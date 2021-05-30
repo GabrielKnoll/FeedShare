3d47a1e7c6958c953aa1940dc5037ee7afa17ef4
Daniel Büchele <daniel@buechele.cc>
2021-05-30 15:38:20 +0100

    copy env

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -7,4 +7,4 @@ DIR="~/feedshare"
 cd "$(dirname "$0")/.."
 yarn build
 rsync -avz --delete public dist prisma package.json yarn.lock $HOST:$DIR
-ssh $HOST 'cd ~/feedshare; yarn install --production; yarn generate:prisma; supervisorctl restart feedshare'
+ssh $HOST 'cd ~/feedshare; yarn install --production; yarn generate:prisma; cp ../.env .env; supervisorctl restart feedshare'
