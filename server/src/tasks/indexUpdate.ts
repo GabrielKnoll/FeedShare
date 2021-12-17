6a18a71152b19e510039254bce59ee7f3997e522
Daniel Büchele <daniel@buechele.cc>
2021-12-17 01:06:22 +0000

    index updater

--- server/src/tasks/indexUpdate.ts
+++ server/src/tasks/indexUpdate.ts
@@ -9,27 +9,33 @@ import sqlite from 'better-sqlite3';
 import {JobHelpers} from 'graphile-worker';
 
 type PodcastImportRow = {
-  id: string;
+  id: number;
   url: string;
   title: string;
-  lastUpdate: string;
+  lastUpdate: number;
   link: string;
-  lastHttpStatus: string;
-  dead: string;
+  lastHttpStatus: number;
+  dead: number;
   contentType: string;
-  itunesId: string;
+  itunesId: number;
   originalUrl: string;
   itunesAuthor: string;
   itunesOwnerName: string;
-  explicit: string;
+  explicit: number;
   imageUrl: string;
   itunesType: string;
   generator: string;
-  newestItemPubdate: string;
+  newestItemPubdate: number;
   language: string;
-  oldestItemPubdate: string;
-  episodeCount: string;
-  popularityScore: string;
+  oldestItemPubdate: number;
+  episodeCount: number;
+  popularityScore: number;
+  createdOn: number;
+  updateFrequency: number;
+  chash: string;
+  host: string;
+  newestEnclosureUrl: string;
+  podcastGuid: string;
 };
 
 const pipeline = promisify(stream.pipeline);
@@ -42,14 +48,6 @@ export default async function (
   _payload: undefined,
   {logger, withPgClient}: JobHelpers,
 ) {
-  const {
-    rows: [lastImport = {value: '0'}],
-  } = await withPgClient((pgClient) =>
-    pgClient.query<{value: string}>(
-      `SELECT value FROM "Config" WHERE key='LastIndexImport'`,
-    ),
-  );
-
   const tmp = tmpdir();
   const tgzFile = join(tmp, 'podcastindex_feeds.db.tgz');
   const dbFile = join(tmp, 'podcastindex_feeds.db');
@@ -107,9 +105,9 @@ export default async function (
 
   logger.info('Starting import...');
   const statement = db.prepare<[number, number]>(
-    `SELECT * FROM Podcasts WHERE
-      itunesAuthor IS NOT NULL AND TRIM(itunesAuthor) != "" AND
-      title        IS NOT NULL AND TRIM(title       ) != ""
+    `SELECT * FROM podcasts WHERE
+      itunesAuthor IS NOT NULL AND TRIM(itunesAuthor) != '' AND
+      title        IS NOT NULL AND TRIM(title       ) != ''
     LIMIT ? OFFSET ?`,
   );
   let offset = 0;
@@ -142,11 +140,11 @@ export default async function (
         ON CONFLICT ("id") DO UPDATE SET (publisher, title, artwork) = (EXCLUDED.publisher, EXCLUDED.title, EXCLUDED.artwork)
         `,
           podcasts.flatMap((p) => [
-            p.id,
-            p.itunesId,
-            p.itunesAuthor,
-            p.title,
-            p.imageUrl,
+            p.id, // can't  be null, primary key
+            !p.itunesId ? null : p.itunesId,
+            p.itunesAuthor, // can't be null, checked in query
+            p.title, // can't be null, checked in query
+            !p.imageUrl ? null : p.imageUrl,
           ]),
         ),
       );
@@ -163,7 +161,7 @@ export default async function (
     pgClient.query(
       `DELETE FROM "Podcast" p
        WHERE
-          "updatedAt" < $1 AND
+          "updatedAt" at time zone current_setting('TIMEZONE') < $1 AND
           NOT EXISTS (SELECT FROM "Episode" WHERE "podcastId" = p.id);`,
       [startTime.toISOString()],
     ),
